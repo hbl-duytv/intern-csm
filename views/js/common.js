6ba93801d3ac6d3fbf907b678a1cfd231226b926
anhnt97 <tuananh05031997@gmail.com>
2019-01-23 10:42:01 +0700

    Ádd common function javascript
    Add common function javascript

---
+++ views/js/common.js
@@ -0,0 +1,41 @@
+function checkUserExist(username,callback){
+    $.ajax({
+        type:'post',
+        url:'http://localhost:8000/check-user-exist',
+        data:{'username':username},
+        dataType:'json',
+        success: function(result){
+            return callback(result.check)
+        }
+    });
+}
+function checkEmailExist(email,callback){
+    $.ajax({
+        type:'post',
+        url:'http://localhost:8000/check-email-exist',
+        data:{'email':email},
+        dataType:'json',
+        success: function(result){
+            return callback(result.check)
+        }
+    });
+}
+function vadidateRegister(id){
+    $("#"+ id).validate({
+		rules: {
+			passwordRegister: { 
+                required: true,
+                minlength: 6,
+                maxlength: 10,
+			} , 
+            confirmPassword: { 
+                equalTo: "#passwordRegister"
+            }
+		},
+		messages:{
+		passwordRegister: { 
+			required:"The password is required"
+		}
+		}
+	});
+}
